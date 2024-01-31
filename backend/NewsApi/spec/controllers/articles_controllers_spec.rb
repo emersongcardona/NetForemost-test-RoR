f96b425b238b03a3519911a9bb5ab1743bcbaaf4
emersongcardona <emersongcardona@gmail.com>
2024-01-31 07:28:53 -0600

    fix tests

--- backend/NewsApi/spec/controllers/articles_controllers_spec.rb
+++ backend/NewsApi/spec/controllers/articles_controllers_spec.rb
@@ -2,28 +2,29 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
     let(:fake_news_response) do 
-        { 'articles' => [{ 'title' => 'Fake Article', 'content' => 'Fake content', 'author' => 'Fake Author' }] }
+        { 'articles' => [{'id' => 1, 'title' => 'Fake Article', 'content' => 'Fake content', 'author' => 'Fake Author' }] }
     end
   describe '#index' do
-    it 'Calls methods in the right way' do
-      allow(NewsApiClient).to receive(:get_news).and_return(fake_news_response)
-      allow(Article).to receive(:count).and_return(20)
+    it 'creates 20 new articles if the count is 0' do
+        Article.destroy_all
+        get :index
+        expect(Article.count).to eq(20)
+      end
 
+    it 'get articles fron index' do
       get :index
-
       expect(response).to have_http_status(:success)
-      expect(NewsApiClient).to have_received(:count).once
-      expect(Article).to have_received(:count).once
+      response_data = JSON.parse(response.body)
+    end
     end
 
-    it 'fetches news articles without creating new records if count is 20 or more' do
-      allow_any_instance_of(NewsApiClient).to receive(:get_news).and_return(fake_news_response)
-      allow(Article).to receive(:count).and_return(20)
-
-      get :index
-
-      expect(response).to have_http_status(:success)
-      expect(Article.count).to eq(20) 
+    describe '#show' do
+        it 'returns JSON of a specific article by ID' do
+          article = create(:article, id: 123, title: 'Test Article', content: 'Test content', author: 'Test Author')
+          get :show, params: { id: article.id }
+          expect(response).to have_http_status(:success)
+          response_data = JSON.parse(response.body)
+          expect(response_data['id']).to eq(article.id)
+        end
     end
-  end
 end
